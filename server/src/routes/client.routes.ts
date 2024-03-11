93d066f64c8d4ce4d8d215fdf0d68d3a915fd999
João Pedro <jp871157@gmail.com>
2024-03-10 21:53:02 -0300

    refactor: altered client table and refactored create client request to support client coordinates

--- server/src/routes/client.routes.ts
+++ server/src/routes/client.routes.ts
@@ -4,8 +4,9 @@ import { verifyClientEmailMiddleware } from "../middlewares/verifyClientEmail.mi
 import { verifyClientPhoneMiddleware } from "../middlewares/verifyClientPhone.middleware";
 import { validateBodyMiddleware } from "../middlewares/validateBody.middleware";
 import { ClientCreateSchema } from "../schemas/client.schema";
+import { verifyClientCoordinates } from "../middlewares/verifyClientCoordinates.middleware";
 
 export const clientRouter: Router = Router()
 
-clientRouter.post('/', validateBodyMiddleware(ClientCreateSchema), verifyClientEmailMiddleware, verifyClientPhoneMiddleware, (req: Request, res: Response) => clientController.create(req, res))
+clientRouter.post('/', validateBodyMiddleware(ClientCreateSchema), verifyClientEmailMiddleware, verifyClientPhoneMiddleware, verifyClientCoordinates, (req: Request, res: Response) => clientController.create(req, res))
 clientRouter.get('/', (req: Request, res: Response) => clientController.list(req, res))
--- server/src/schemas/client.schema.ts
+++ server/src/schemas/client.schema.ts
@@ -3,5 +3,7 @@ import { z } from "zod";
 export const ClientCreateSchema = z.object({
   nome: z.string().max(50),
   email: z.string().email(),
-  telefone: z.string().length(11)
+  telefone: z.string().length(11),
+  coordinate_x: z.number(),
+  coordinate_y: z.number()
 })
